c431ed503b8510fe79918abf959339ceda16f5f4
Aniksaha-ui <sahaanik1045@gmail.com>
2022-05-02 02:26:03 +0600

    create jwt token

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+var jwt = require("jsonwebtoken");
 const port = process.env.PORT || 4000;
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
@@ -24,12 +25,23 @@ async function run() {
     await client.connect();
     const productCollection = client.db("inventory").collection("product");
 
+    app.post("/login", async (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN, {
+        expiresIn: "1d",
+      });
+      console.log(user);
+      res.send({ accessToken });
+    });
+
+    //Products
     app.get("/product", async (req, res) => {
       const query = {};
       const productCursor = productCollection.find(query);
       const product = await productCursor.toArray();
       res.send(product);
     });
+    //Products
   } finally {
   }
 }
