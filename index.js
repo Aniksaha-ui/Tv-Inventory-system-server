2fa6bb1b91db73fdf6570d1c4d97d19480724742
Aniksaha-ui <sahaanik1045@gmail.com>
2022-05-06 23:39:32 +0600

    insert product api create

--- index.js
+++ index.js
@@ -66,6 +66,13 @@ async function run() {
       res.send(product);
     });
 
+    //insert new product
+    app.post("/product", async (req, res) => {
+      const newProduct = req.body;
+      const result = await productCollection.insertOne(newProduct);
+      res.send(result);
+    });
+
     //delete a product
     app.delete("/product/:id", async (req, res) => {
       const id = req.params.id;
